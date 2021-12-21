471f92b85f6c499cdf8aebcaa42bbe968bd22e48
Ruan Luies <omega@live.co.za>
2021-12-21 15:13:01 +0200

    add convenience scripts for development.

---
+++ download_dependencies_devel.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+git clone https://github.com/coin-or-tools/BuildTools
+git clone https://github.com/baggins800/Cbc
+git clone https://github.com/coin-or/Osi
+git clone https://github.com/coin-or/Clp
+git clone https://github.com/coin-or/Cgl
+git clone https://github.com/coin-or/CoinUtils
---
+++ remove_dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+rm -rf BuildTools
+rm -rf Cbc
+rm -rf Osi
+rm -rf CoinUtils
+rm -rf Clp
+rm -rf Cgl
+
+
